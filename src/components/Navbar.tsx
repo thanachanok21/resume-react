a6ace8f5b3b260b14840747b18bb4ef5af0cde66
thanachanok21 <tncnp27402@gmail.com>
2024-08-17 23:52:56 +0700

    finish

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./navbar.css";
 
 const Navbar: React.FC = () => {
@@ -6,11 +6,48 @@ const Navbar: React.FC = () => {
   const menuRef = useRef<HTMLDivElement | null>(null);
   const navlistRef = useRef<HTMLUListElement | null>(null);
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+  
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (headerRef.current) {
+  //       headerRef.current.classList.toggle("sticky", window.scrollY > 100);
+  //     }
+  //   };
+
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
+  // const handleMenuClick = () => {
+  //   if (menuRef.current && navlistRef.current) {
+  //     menuRef.current.classList.toggle("bx-x");
+  //     navlistRef.current.classList.toggle("open");
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   const handleWindowScroll = () => {
+  //     if (menuRef.current && navlistRef.current) {
+  //       menuRef.current.classList.remove("bx-x");
+  //       navlistRef.current.classList.remove("open");
+  //     }
+  //   };
+
+  //   window.addEventListener("scroll", handleWindowScroll);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", handleWindowScroll);
+  //   };
+  // }, []);
+
   useEffect(() => {
     const handleScroll = () => {
-      if (headerRef.current) {
-        headerRef.current.classList.toggle("sticky", window.scrollY > 100);
-      }
+      setIsSticky(window.scrollY > 100);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -21,18 +58,12 @@ const Navbar: React.FC = () => {
   }, []);
 
   const handleMenuClick = () => {
-    if (menuRef.current && navlistRef.current) {
-      menuRef.current.classList.toggle("bx-x");
-      navlistRef.current.classList.toggle("open");
-    }
+    setIsMenuOpen(!isMenuOpen);
   };
 
   useEffect(() => {
     const handleWindowScroll = () => {
-      if (menuRef.current && navlistRef.current) {
-        menuRef.current.classList.remove("bx-x");
-        navlistRef.current.classList.remove("open");
-      }
+      setIsMenuOpen(false);
     };
 
     window.addEventListener("scroll", handleWindowScroll);
@@ -45,34 +76,33 @@ const Navbar: React.FC = () => {
   return (
     <>
       {/* navbar */}
-      <header ref={headerRef}>
-        <a href="#" className="logo">
-          Pr<span>e</span>m.
-        </a>
-        <ul className="navlist" id="navlist">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#mySkills">My skills</a>
-          </li>
-          <li>
-            <a href="#portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="#contactMe">Contact Me</a>
-          </li>
-        </ul>
-        <div
-          className="bx bx-menu"
-          id="menu-icon"
-          ref={menuRef}
-          onClick={handleMenuClick}
-        ></div>
-      </header>
+      <header className={isSticky ? "sticky" : ""}>
+      <a href="#" className="logo">
+        Pr<span>e</span>m.
+      </a>
+      <ul className={`navlist ${isMenuOpen ? "open" : ""}`} id="navlist">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#mySkills">My skills</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contactMe">Contact Me</a>
+        </li>
+      </ul>
+      <div
+        className={`bx bx-menu ${isMenuOpen ? "bx-x" : ""}`}
+        id="menu-icon"
+        onClick={handleMenuClick}
+      ></div>
+    </header>
     </>
   );
 };
